02f5745359fca72191ed5a309c9cedb18d21fe9e
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2022-01-19 22:36:00 +0900

    listView作成中

--- NoteApp/NoteApp/TopForm.Designer.cs
+++ NoteApp/NoteApp/TopForm.Designer.cs
@@ -29,20 +29,82 @@ namespace NoteApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.listViewNote = new System.Windows.Forms.ListView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listViewNote
+            // 
+            this.listViewNote.HideSelection = false;
+            this.listViewNote.Location = new System.Drawing.Point(12, 12);
+            this.listViewNote.Name = "listViewNote";
+            this.listViewNote.Size = new System.Drawing.Size(776, 374);
+            this.listViewNote.TabIndex = 0;
+            this.listViewNote.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(45, 392);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(93, 39);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "新規";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(178, 392);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(90, 39);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "編集";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(307, 392);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(87, 39);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "削除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(427, 392);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(85, 39);
+            this.buttonExport.TabIndex = 4;
+            this.buttonExport.Text = "出力";
+            this.buttonExport.UseVisualStyleBackColor = true;
             // 
             // TopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listViewNote);
             this.Name = "TopForm";
             this.Text = "NoteApp";
+            this.Load += new System.EventHandler(this.TopForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewNote;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonExport;
     }
 }
 
