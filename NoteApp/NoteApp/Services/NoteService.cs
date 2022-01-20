f1c867df9d3366880ad550890b583db787376c66
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2022-01-20 22:09:03 +0900

    新規作成ボタンクリック時のイベント作成

---
+++ NoteApp/NoteApp/Services/NoteService.cs
@@ -0,0 +1,14 @@
+﻿using NoteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Services
+{
+    class NoteService
+    {
+        public List<NoteModel> notes;
+    }
+}
